0755d03e38d2ada7d4d91769085ae75478ad3a33
itto <121813459+yteau@users.noreply.github.com>
2023-03-20 16:57:52 +0100

    Add files via upload

---
+++ boucles et fonctions.cpp
@@ -0,0 +1,47 @@
+# Boucle for
+for i in range(1, 11):
+    print(i)
+
+# Boucle while
+i = 1
+while i <= 10:
+    print(i)
+    i += 1
+
+# Fonction
+def addition(a, b):
+    return a + b
+
+// Boucle for
+for (let i = 1; i <= 10; i++) {
+    console.log(i);
+}
+
+// Boucle while
+let i = 1;
+while (i <= 10) {
+    console.log(i);
+    i++;
+}
+
+// Fonction
+function addition(a, b) {
+    return a + b;
+}
+
+// Boucle for
+for (int i = 1; i <= 10; i++) {
+    cout << i << endl;
+}
+
+// Boucle while
+int i = 1;
+while (i <= 10) {
+    cout << i << endl;
+    i++;
+}
+
+// Fonction
+int addition(int a, int b) {
+    return a + b;
+}
